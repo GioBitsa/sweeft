d494eb4e0fe1c7efbaef8d4d00fb04264f8574ec
Giorgi <giorgi.bitsadze01@gmail.com>
2021-12-16 23:37:43 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import react, {useState} from 'react'
+import UserList from './components/UserList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import UserPage from './components/UserPage';
 
-function App() {
+
+const App = () => {
+
+  const [id, setId] = useState()
+
+  const childItem = (item) => {
+    setId(item)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app'>
+        <Routes>
+          <Route exact path="/" element={<UserList childItem={childItem} />} />
+          <Route exact path={`/user/${id}`} element={<UserPage childItem={childItem} id={id} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
